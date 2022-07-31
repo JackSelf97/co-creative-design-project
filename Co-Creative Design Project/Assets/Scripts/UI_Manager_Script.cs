81b41e456e500bbd8016c86c8bbb401f91ce94c6
JackSelf97 <selfbrothers@hotmail.com>
2022-07-31 18:00:14 +0100

    UI & Scenes (0.3)
    - Added true loading screen
    - Can be worked on
    - Need to add fake loading screen

---
+++ Co-Creative Design Project/Assets/Scripts/UI_Manager_Script.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+// https://www.youtube.com/watch?v=YMj2qPq9CP8&t=274s&ab_channel=Brackeys
+public class UI_Manager_Script : MonoBehaviour
+{
+    public GameObject loadingScreen;
+    public Slider loadingBar;
+    public Text progressTxt;
+
+    public void SwitchScenes(string sceneName)
+    {
+        if (sceneName == "Quit")
+        {
+            Application.Quit();
+            Debug.Log("Quiting game...");
+            return;
+        }
+
+        StartCoroutine(LoadAsynchronously(sceneName));
+    }
+
+    IEnumerator LoadAsynchronously(string sceneName)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName); // store the status
+        loadingScreen.SetActive(true);
+        while (!operation.isDone)
+        {
+
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            Debug.Log(progress);
+            loadingBar.value = progress;
+            progressTxt.text = progress * 100f + "%";
+
+            yield return null; // wait a frame
+        }
+    }
+
+}
