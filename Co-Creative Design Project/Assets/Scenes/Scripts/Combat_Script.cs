898e4bf4f7e3fdbc8274792ec4a06c07af39a21b
achitchen <61246622+achitchen@users.noreply.github.com>
2022-08-10 14:40:10 +0100

    Added animations for all units

---
+++ Co-Creative Design Project/Assets/Scenes/Scripts/Combat_Script.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Combat_Script : MonoBehaviour
+{
+    public enum UnitType { playerArcher, playerSpear, playerDagger, enemyMelee, enemyRanged, enemyHealer};
+    [SerializeField] UnitType unitType;
+    [SerializeField] GameObject projectile;
+    [SerializeField] float damageValue = 3;
+    public Animator animator;
+    private ParticleSystem deathParticle;
+    public ParticleSystem damageParticle;
+    public float healthValue = 10;
+    public float maxHealth;
+    public bool canTeleport = false;
+   
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        animator.SetTrigger("Idle");
+        maxHealth = healthValue;
+        deathParticle = this.transform.Find("Dust_Particles").GetComponent<ParticleSystem>();
+        damageParticle = this.transform.Find("Boom_Particle").GetComponent<ParticleSystem>();
+    }
+
+    private void Update()
+    {
+        if (healthValue <= 0)
+        {
+            GameManager.gMan.enemyList.Remove(gameObject); // remove enemy from list
+            //play death particles. Destroy unit after a short delay;
+            if (!deathParticle.isPlaying)
+            {
+                deathParticle.Play();
+            }
+            StartCoroutine(UnitDeath());
+        }
+    }
+
+    public void DetermineAttack(GameObject target)
+    {
+        if (target != null)
+        {
+            if (target.GetComponent<Combat_Script>().healthValue > 0)
+            {
+                //check type of unit
+                //call Attack script attached to the gameObject based on the unit type
+                switch (unitType)
+                {
+                    case UnitType.playerArcher:
+                        FireProjectile(target);
+                        break;
+                    case UnitType.playerSpear:
+                        MeleeAttack(target);
+                        break;
+                    case UnitType.enemyMelee:
+                        MeleeAttack(target);
+                        break;
+                    case UnitType.enemyRanged:
+                        FireProjectile(target);
+                        break;
+                    case UnitType.enemyHealer:
+                        HealUnit(target);
+                        break;
+                    case UnitType.playerDagger:
+                        MeleeAttack(target);
+                        break;
+                }
+            }
+        }
+    }
+
+    private void HealUnit(GameObject target)
+    {
+            target.GetComponent<Combat_Script>().healthValue += damageValue;
+            Debug.Log("Enemy Healed");
+    }
+
+    private void FireProjectile(GameObject target)
+    {
+        projectile.GetComponent<Projectile_Script>().target = target;
+        Instantiate(projectile, this.gameObject.transform);
+        animator.SetTrigger("Idle");
+        animator.ResetTrigger("Attack");
+        animator.ResetTrigger("Hit");
+    }
+
+    private void MeleeAttack(GameObject target)
+    {
+        target.GetComponent<Combat_Script>().damageParticle.Play();
+        target.GetComponent<Combat_Script>().healthValue -= damageValue;
+        animator.SetTrigger("Idle");
+        animator.ResetTrigger("Attack");
+        target.GetComponent<Combat_Script>().animator.SetTrigger("Hit");
+        target.GetComponent<Combat_Script>().animator.ResetTrigger("Attack");
+        target.GetComponent<Combat_Script>().animator.ResetTrigger("Idle");
+    }
+
+    public void Teleport(GameObject target)
+    {
+        if (target != null)
+        {
+            Vector3 offset = new Vector3(1, 0, 0);
+            gameObject.transform.position = target.transform.position + offset;
+            canTeleport = false;
+        }
+    }
+
+    IEnumerator UnitDeath()
+    {
+        yield return new WaitForSeconds(1f);
+        Destroy(gameObject);
+    }
+}
---
+++ Co-Creative Design Project/Assets/Scenes/Scripts/Movement_Script.cs
@@ -0,0 +1,187 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Movement_Script : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] float attackRange;
+    [SerializeField] float attackDelay = 1f;
+    [SerializeField] bool isHealer = false;
+    [SerializeField] bool isDaggerUnit = false;
+    private ParticleSystem assassinParticles;
+    float targetDistance;
+    float distance;
+    string unitTag;
+    string playerTag = "Player";
+    string enemyTag = "Enemy";
+    bool canAttack = true;
+    Vector3 moveDir;
+    GameObject target;
+    GameObject[] potentialTargets;
+
+    
+
+
+
+    void Start()
+    {
+        //denotes tag attached to current gameObject
+        unitTag = gameObject.tag;
+        GetPossibleTargets();
+        if(isDaggerUnit)
+        {
+            assassinParticles = this.transform.Find("Assassin_Particles").GetComponent<ParticleSystem>();
+        }
+    }
+
+    private void Update()
+    {
+        if (target != null)
+        {
+            //if the target unit is in range, trigger the attack
+            distance = (target.transform.position - gameObject.transform.position).sqrMagnitude;
+            if (distance <= attackRange)
+            {
+                if (canAttack)
+                {
+                    //if the unit is a healer, only heal units if they are below their maximum health
+                    if (isHealer)
+                    {
+                        if (target.GetComponent<Combat_Script>().healthValue < target.GetComponent<Combat_Script>().maxHealth)
+                        {
+                            StartCoroutine(AttackDelay());
+                            canAttack = false;
+                        }
+                    }
+                    else
+                    {
+                        StartCoroutine(AttackDelay());
+                        canAttack = false;
+                    }
+                }
+            }
+
+            else
+            {
+                //if unit is a dagger unit, use teleport ability
+                if (isDaggerUnit)
+                {
+                    if (GetComponent<Combat_Script>().canTeleport)
+                    {
+                        if (!assassinParticles.isPlaying)
+                        {
+                            assassinParticles.Play();
+                        }
+                        StartCoroutine(TeleportDelay());
+                    }
+                    else
+                    {
+                        //move towards target unit
+                        moveDir = (target.transform.position - gameObject.transform.position);
+                        transform.Translate(moveDir * speed * Time.deltaTime);
+                    }
+                }
+                else
+                {
+                    //move towards target unit
+                    moveDir = (target.transform.position - gameObject.transform.position);
+                    transform.Translate(moveDir * speed * Time.deltaTime);
+                }
+            }
+            //make unit face backwards if the target is behind it
+
+                if (target.transform.position.x < gameObject.transform.position.x)
+                {
+                    GetComponent<SpriteRenderer>().flipX = true;
+                }
+                else
+                {
+                    GetComponent<SpriteRenderer>().flipX = false;
+                }
+        }
+        else
+        {
+            GetPossibleTargets();
+        }
+    }
+
+    private IEnumerator AttackDelay()
+    {
+        GetComponent<Combat_Script>().animator.ResetTrigger("Idle");
+        GetComponent<Combat_Script>().animator.ResetTrigger("Hit");
+        GetComponent<Combat_Script>().animator.SetTrigger("Attack");
+
+        yield return new WaitForSeconds(attackDelay);
+        gameObject.GetComponent<Combat_Script>().DetermineAttack(target);
+        canAttack = true;
+    }
+
+    private IEnumerator TeleportDelay()
+    {
+        yield return new WaitForSeconds(attackDelay);
+        GetComponent<Combat_Script>().Teleport(target);
+    }
+
+    //compare distance to find nearest unit and store as target
+    private void DetermineTarget()
+    {
+        Dictionary<float, GameObject> targetDistances = new Dictionary<float, GameObject>();
+        foreach (GameObject opposingUnit in potentialTargets)
+        {
+            float distance = (gameObject.transform.position - opposingUnit.transform.position).sqrMagnitude;
+            targetDistances.Add(distance, opposingUnit);
+        }
+        List<float> distances = targetDistances.Keys.ToList();
+        //if unit is a healer, get the furthest possible target
+        if (isHealer || isDaggerUnit)
+        {
+            targetDistance = distances.Max();
+        }
+        else
+        {
+            targetDistance = distances.Min();
+        }
+        target = targetDistances[targetDistance];
+        
+    }
+
+
+    private void GetPossibleTargets()
+    {
+        //if unit is a healer, find objects with the same tag
+        if (isHealer)
+        {
+            if (unitTag == playerTag)
+            {
+                potentialTargets = GameObject.FindGameObjectsWithTag(playerTag);
+            }
+            else if (unitTag == enemyTag)
+            {
+                potentialTargets = GameObject.FindGameObjectsWithTag(enemyTag);
+            }
+        }
+        else
+        {
+            //find objects with opposite tag
+            if (unitTag == playerTag)
+            {
+                //if the unit is a dagger unit, enable their teleport when selecting a target
+                if (isDaggerUnit)
+                {
+                    GetComponent<Combat_Script>().canTeleport = true;
+                }
+                potentialTargets = GameObject.FindGameObjectsWithTag(enemyTag);
+            }
+            else if (unitTag == enemyTag)
+            {
+                potentialTargets = GameObject.FindGameObjectsWithTag(playerTag);
+            }
+        }
+        if (potentialTargets.Length != 0)
+        {
+            DetermineTarget();
+        }
+    }
+}
