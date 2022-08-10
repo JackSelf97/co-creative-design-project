9fed33f29b0b5678233b5e125150c8a3b98252ba
VampireViktor20 <kylecornw3ll@outlook.com>
2022-08-10 01:18:48 +0100

    Shop UI (0.2) and Inventory UI (0.1)
    - Shop and Inventory Designs completed. Mechanics to apply tomorrow.
    - MP3 Sound Files Imported ready for use.
    - Loading Screen Updated with Rotating Object above slider bar.

---
+++ Co-Creative Design Project/Assets/Scripts/LoadingIcon_Script.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//https://www.youtube.com/watch?v=MFyrt3t5nXU
+public class LoadingIcon_Script : MonoBehaviour
+{
+    private float rotZ;
+    public float rotationSpeed;
+   
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotZ += Time.deltaTime * rotationSpeed; // Rotation of loading icon on Z Axis by set speed
+
+        transform.rotation = Quaternion.Euler(0, 0, rotZ);
+    }
+}
